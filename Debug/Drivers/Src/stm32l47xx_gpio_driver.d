b4f00773da04365a923055a193bdc58aced1ef61
harshilpatel847 <harshil.thevain@gmail.com>
2025-03-28 14:10:30 -0400

    Add .gitignore for Eclipse IDE files and build output

--- Debug/Drivers/Src/stm32l47xx_gpio_driver.d
+++
@@ -1,4 +0,0 @@
-Drivers/Src/stm32l47xx_gpio_driver.o: \
- ../Drivers/Src/stm32l47xx_gpio_driver.c \
- C:/Users/harshilpatel/Downloads/Dev/Embedded-C/My_workspace/target/stm32l47xx_essential_drivers/Drivers/Inc/stm32l47xx_gpio_driver.h
-C:/Users/harshilpatel/Downloads/Dev/Embedded-C/My_workspace/target/stm32l47xx_essential_drivers/Drivers/Inc/stm32l47xx_gpio_driver.h:
--- Debug/Src/main.d
+++
@@ -1,3 +0,0 @@
-Src/main.o: ../Src/main.c \
- C:/Users/harshilpatel/Downloads/Dev/Embedded-C/My_workspace/target/stm32l47xx_essential_drivers/Drivers/Inc/stm32l47xx.h
-C:/Users/harshilpatel/Downloads/Dev/Embedded-C/My_workspace/target/stm32l47xx_essential_drivers/Drivers/Inc/stm32l47xx.h:
